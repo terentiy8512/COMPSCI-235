2f8679128e386018acdeb8ae268e67d8b2565a9c
Vadim <vadoq8512@gmail.com>
2020-08-22 12:09:38 +1200

    Added greeting request handler

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from markupsafe import escape
 
 
@@ -33,4 +33,8 @@ def create_app():
     def shows_news():
         return "Today’s news is ..."
 
+    @app.route('/greeting')
+    def show_greeting():
+        name = request.args.get('name')
+        return '<p>Kia Ora {}!'.format(name)
     return app
